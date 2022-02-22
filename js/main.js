c45ef5fa0736846c84ad598ae6761cca9d17e26f
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2022-02-22 21:26:21 +0900

    영상 ios 자동 재생 추가

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ window.addEventListener("DOMContentLoaded", (e) => {
   let video = document.getElementById("main_video");
 
   video.addEventListener("loadeddata", () => {
-    video.play();
+    video.pause();
+    setTimeout(() => {
+      video.play();
+    }, 500);
   })
 
  
